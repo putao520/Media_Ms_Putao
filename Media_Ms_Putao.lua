4c08f5dc407601c0bd6ccb70a16ecfc218482823
putao520 <putao520@putao282.com>
2024-12-16 11:32:59 +0800

    first commit

---
+++ Media_Ms_Putao.lua
@@ -0,0 +1,16 @@
+local LSM = LibStub("LibSharedMedia-3.0")
+
+print("|cffff5a9b-------------葡萄媒体包已载入!-------------|r")
+
+LSM:Register("sound", "[putao]阴暗面之力已结束", [[Interface\Addons\Media_Ms_Putao\Sound\[putao]阴暗面之力已结束.mp3]])
+LSM:Register("sound", "[putao]全神盾已耗尽", [[Interface\Addons\Media_Ms_Putao\Sound\[putao]全神盾已耗尽.mp3]])
+LSM:Register("sound", "[putao]死亡之风即将消失", [[Interface\Addons\Media_Ms_Putao\Sound\[putao]死亡之风即将消失.mp3]])
+LSM:Register("sound", "[putao]神言已就绪", [[Interface\Addons\Media_Ms_Putao\Sound\[putao]神言已就绪.mp3]])
+LSM:Register("sound", "[putao]瞬发快疗", [[Interface\Addons\Media_Ms_Putao\Sound\[putao]瞬发快疗.mp3]])
+LSM:Register("sound", "[putao]瞬发治疗", [[Interface\Addons\Media_Ms_Putao\Sound\[putao]瞬发治疗.mp3]])
+LSM:Register("sound", "[putao]洞察预兆", [[Interface\Addons\Media_Ms_Putao\Sound\[putao]洞察预兆.mp3]])
+LSM:Register("sound", "[putao]虔诚预兆", [[Interface\Addons\Media_Ms_Putao\Sound\[putao]虔诚预兆.mp3]])
+LSM:Register("sound", "[putao]慰藉预兆", [[Interface\Addons\Media_Ms_Putao\Sound\[putao]慰藉预兆.mp3]])
+LSM:Register("sound", "[putao]远见预兆", [[Interface\Addons\Media_Ms_Putao\Sound\[putao]远见预兆.mp3]])
+LSM:Register("sound", "[putao]暗影魔已结束", [[Interface\Addons\Media_Ms_Putao\Sound\[putao]暗影魔已结束.mp3]])
+LSM:Register("sound", "[putao]快用命", [[Interface\Addons\Media_Ms_Putao\Sound\[putao]快用命.mp3]])
