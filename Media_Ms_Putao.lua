0159f4e05d6878fbdf9a8dfa259058b4937c82cd
putao520 <putao520@putao282.com>
2024-12-19 15:19:44 +0800

    修改BUG

--- Media_Ms_Putao.lua
+++ Media_Ms_Putao.lua
@@ -9,7 +9,7 @@ if( v1 == "牧师") then
 	LSM:Register("sound", "[putao]瞬发治疗", [[Interface\Addons\Media_Ms_Putao\Sound\[putao]瞬发治疗.mp3]])
 	LSM:Register("sound", "[putao]洞察预兆", [[Interface\Addons\Media_Ms_Putao\Sound\[putao]洞察预兆.mp3]])
 	LSM:Register("sound", "[putao]虔诚预兆", [[Interface\Addons\Media_Ms_Putao\Sound\[putao]虔诚预兆.mp3]])
-	LSM:Register("sound", "[putao]远见预兆消失", [[Interface\Addons\Media_Ms_Putao\Sound\[putao]远见预兆消失.mp3]])
+	LSM:Register("sound", "[putao]虔诚预兆消失", [[Interface\Addons\Media_Ms_Putao\Sound\[putao]虔诚预兆消失.mp3]])
 	LSM:Register("sound", "[putao]慰藉预兆", [[Interface\Addons\Media_Ms_Putao\Sound\[putao]慰藉预兆.mp3]])
 	LSM:Register("sound", "[putao]远见预兆", [[Interface\Addons\Media_Ms_Putao\Sound\[putao]远见预兆.mp3]])
 	LSM:Register("sound", "[putao]远见预兆消失", [[Interface\Addons\Media_Ms_Putao\Sound\[putao]远见预兆消失.mp3]])
